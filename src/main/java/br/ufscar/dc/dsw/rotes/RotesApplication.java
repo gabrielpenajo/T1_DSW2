d0c96dd197d36fd741bda30d19386c9670c67a31
gabrielpenajo <gabrielpenajo13@gmail.com>
2022-08-26 10:55:10 -0300

    initial commit

---
+++ src/main/java/br/ufscar/dc/dsw/rotes/RotesApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.rotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufscar/dc/dsw/rotes/RotesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.rotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
