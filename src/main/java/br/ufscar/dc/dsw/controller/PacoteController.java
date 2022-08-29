2456fa7bb48f1bac1947c50ec581258f8948f5c7
gabrielpenajo <gabrielpenajo13@gmail.com>
2022-08-29 20:39:30 -0300

    refactor pacote controller

--- src/main/java/br/ufscar/dc/dsw/controller/PacoteController.java
+++ src/main/java/br/ufscar/dc/dsw/controller/PacoteController.java
@@ -15,71 +15,71 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.ufscar.dc.dsw.domain.Editora;
-import br.ufscar.dc.dsw.domain.Livro;
-import br.ufscar.dc.dsw.service.spec.IEditoraService;
-import br.ufscar.dc.dsw.service.spec.ILivroService;
+import br.ufscar.dc.dsw.domain.Agencia;
+import br.ufscar.dc.dsw.domain.Pacote;
+import br.ufscar.dc.dsw.service.spec.IAgenciaService;
+import br.ufscar.dc.dsw.service.spec.IPacoteService;
 
 @Controller
-@RequestMapping("/livros")
-public class LivroController {
+@RequestMapping("/pacotes")
+public class PacoteController {
 
 	@Autowired
-	private ILivroService livroService;
+	private IPacoteService pacoteService;
 
 	@Autowired
-	private IEditoraService editoraService;
+	private IAgenciaService agenciaService;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar(Livro livro) {
-		return "livro/cadastro";
+	public String cadastrar(Pacote pacote) {
+		return "pacote/cadastro";
 	}
 
 	@GetMapping("/listar")
 	public String listar(ModelMap model) {
-		model.addAttribute("livros", livroService.buscarTodos());
-		return "livro/lista";
+		model.addAttribute("pacotes", pacoteService.buscarTodos());
+		return "pacote/lista";
 	}
 
 	@PostMapping("/salvar")
-	public String salvar(@Valid Livro livro, BindingResult result, RedirectAttributes attr) {
+	public String salvar(@Valid Pacote pacote, BindingResult result, RedirectAttributes attr) {
 
 		if (result.hasErrors()) {
-			return "livro/cadastro";
+			return "pacote/cadastro";
 		}
 
-		livroService.salvar(livro);
-		attr.addFlashAttribute("sucess", "livro.create.sucess");
-		return "redirect:/livros/listar";
+		pacoteService.salvar(pacote);
+		attr.addFlashAttribute("sucess", "pacote.create.sucess");
+		return "redirect:/pacotes/listar";
 	}
 
 	@GetMapping("/editar/{id}")
 	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
-		model.addAttribute("livro", livroService.buscarPorId(id));
-		return "livro/cadastro";
+		model.addAttribute("pacote", pacoteService.buscarPorId(id));
+		return "pacote/cadastro";
 	}
 
 	@PostMapping("/editar")
-	public String editar(@Valid Livro livro, BindingResult result, RedirectAttributes attr) {
+	public String editar(@Valid Pacote pacote, BindingResult result, RedirectAttributes attr) {
 
 		if (result.hasErrors()) {
-			return "livro/cadastro";
+			return "pacote/cadastro";
 		}
 
-		livroService.salvar(livro);
-		attr.addFlashAttribute("sucess", "livro.edit.sucess");
-		return "redirect:/livros/listar";
+		pacoteService.salvar(pacote);
+		attr.addFlashAttribute("sucess", "pacote.edit.sucess");
+		return "redirect:/pacotes/listar";
 	}
 
 	@GetMapping("/excluir/{id}")
 	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
-		livroService.excluir(id);
-		attr.addFlashAttribute("sucess", "livro.delete.sucess");
-		return "redirect:/livros/listar";
+		pacoteService.excluir(id);
+		attr.addFlashAttribute("sucess", "pacote.delete.sucess");
+		return "redirect:/pacotes/listar";
 	}
 
-	@ModelAttribute("editoras")
-	public List<Editora> listaEditoras() {
-		return editoraService.buscarTodos();
+	@ModelAttribute("agencias")
+	public List<Agencia> listaEditoras() {
+		return agenciaService.buscarTodos();
 	}
 }
