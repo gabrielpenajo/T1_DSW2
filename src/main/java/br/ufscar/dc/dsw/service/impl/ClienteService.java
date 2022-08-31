06883aaba2994baab21c2d6300fbc4d753231b4e
Matheus Mattioli <matheus.mattioli@hotmail.com>
2022-08-31 15:11:16 -0300

    Tentando compilar

--- src/main/java/br/ufscar/dc/dsw/service/impl/ClienteService.java
+++ src/main/java/br/ufscar/dc/dsw/service/impl/ClienteService.java
@@ -2,12 +2,17 @@ package br.ufscar.dc.dsw.service.impl;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
 
 import br.ufscar.dc.dsw.dao.IClienteDAO;
 import br.ufscar.dc.dsw.domain.Cliente;
 import br.ufscar.dc.dsw.service.spec.IClienteService;
 
+@Service
+@Transactional(readOnly = false)
 public class ClienteService implements IClienteService {
 
     @Autowired
