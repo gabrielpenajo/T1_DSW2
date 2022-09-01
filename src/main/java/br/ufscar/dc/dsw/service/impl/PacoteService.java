1dff6a9afe50b5d64362da6462d069275898dd88
Matheus Mattioli <matheus.mattioli@hotmail.com>
2022-09-01 12:29:14 -0300

    Filtros pacote service concluido

--- src/main/java/br/ufscar/dc/dsw/service/impl/PacoteService.java
+++ src/main/java/br/ufscar/dc/dsw/service/impl/PacoteService.java
@@ -96,14 +96,32 @@ public class PacoteService implements IPacoteService {
 
 	@Override
 	public List<Pacote> buscarPorDestino(String destino) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Pacote> pacotes = this.buscarTodos();
+
+		pacotes = pacotes.stream().filter(x -> x.getCidade() == destino).filter(x -> x.getEstado() == destino).filter(x -> x.getPais() == destino).collect(Collectors.toList());
+
+		return pacotes;
 	}
 
 	@Override
 	public List<Pacote> buscarEAplicarFltros(String destino, Agencia a, Date dataPartida, String validoStr) {
-		// TODO Auto-generated method stub
-		return null;
+		// todos os filtros juntos
+		List<Pacote> pacotes = this.buscarTodos();
+
+		Boolean valido = validoStr.equals("on") ? true : false;
+
+		Date hoje = Date.from(Instant.now());
+		if (dataPartida != null)
+			pacotes = pacotes.stream().filter(x -> x.getDataPartida().toString().equals(dataPartida)).collect(Collectors.toList());
+		if (a != null)
+			pacotes = pacotes.stream().filter(x -> x.getAgencia().equals(a)).collect(Collectors.toList());
+		if (destino != "")
+			pacotes = pacotes.stream().filter(x -> x.getCidade().equals(destino) || x.getEstado().equals(destino) || x.getPais().equals(destino)).collect(Collectors.toList());
+		if (valido == true)
+			pacotes = pacotes.stream().filter(x -> x.getDataPartida().after(hoje)).collect(Collectors.toList());
+
+		return pacotes;
 	}
 
 	
