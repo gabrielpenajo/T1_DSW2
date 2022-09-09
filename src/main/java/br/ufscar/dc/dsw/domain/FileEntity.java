6c313113ab447ccb025c8c15a1de7579bf4e1d00
Matheus Mattioli <matheus.mattioli@hotmail.com>
2022-09-09 17:42:05 -0300

    Arrumando cagada

--- src/main/java/br/ufscar/dc/dsw/domain/FileEntity.java
+++ src/main/java/br/ufscar/dc/dsw/domain/FileEntity.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name = "Cliente")
+@Table(name = "FileDB")
 public class FileEntity extends AbstractEntity<Long> {
 
     @Column(nullable = false, length = 100)
